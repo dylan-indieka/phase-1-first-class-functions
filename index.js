a612fa67fc04e5a1d12e43af1696bc51944311dd
dylan-indieka <dylan.indieka@student.moringaschool.com>
2025-03-04 00:45:28 -0800

    adding files

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+        // ...function body...
+    };
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        // ...function body...
+    };
+}
